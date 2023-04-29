f76db0a0441d1f2c79af81e7845e668a1d1f65a1
JadhavC07 <cjaddhav@gmail.com>
2023-04-29 13:17:11 +0530

    Initial commit

--- src/Components/Validation.js
+++ src/Components/Validation.js
@@ -1,20 +1,57 @@
-
-
+import "./Validation.css"
 
 const Validation = () => {
-
-
-
-    return(
-        <> 
-        <p>hello</p>
-        </>
-    )
-}
-
-// npm uninstall --save  @mui/material @emotion/react @emotion/styled
-// npm uninstall --save @mui/material @mui/styled-engine-sc styled-components
-
-
+  return (
+    <div className="container-sm my-5 box">
+      <div class="row">
+        <div class="col">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="First name"
+            aria-label="First name"
+          />
+        </div>
+        <div class="col">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Last name"
+            aria-label="Last name"
+          />
+        </div>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlInput1" className="form-label">
+          Email address
+        </label>
+        <input
+          type="email"
+          className="form-control"
+          id="exampleFormControlInput1"
+          placeholder="name@example.com"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlTextarea1" className="form-label">
+          Example textarea
+        </label>
+        <textarea
+          className="form-control"
+          id="exampleFormControlTextarea1"
+          rows="3"
+        ></textarea>
+      </div>
+      <div className="mb-3 d-flex justify-content-between size">
+        <button type="button" class="btn btn-primary">
+          Primary
+        </button>
+        <button type="button" class="btn btn-secondary">
+          Secondary
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Validation;
