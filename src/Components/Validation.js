5a63313ca0badce98a8de270b20867d1524e19df
JadhavC07 <cjaddhav@gmail.com>
2023-04-29 12:57:33 +0530

    Starting new one

---
+++ src/Components/Validation.js
@@ -0,0 +1,20 @@
+
+
+
+const Validation = () => {
+
+
+
+    return(
+        <> 
+        <p>hello</p>
+        </>
+    )
+}
+
+// npm uninstall --save  @mui/material @emotion/react @emotion/styled
+// npm uninstall --save @mui/material @mui/styled-engine-sc styled-components
+
+
+
+export default Validation;
